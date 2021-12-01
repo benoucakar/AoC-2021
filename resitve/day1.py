d9b4cbdb8b487074cb263125f5bf1cbe6406fe87
Beno Učakar <beno.ucakar@gmail.com>
2021-12-01 17:37:43 +0100

    1. dan

---
+++ resitve/day1.py
@@ -0,0 +1,23 @@
+meritve = []
+with open("input/day1.txt") as inp:
+    for vrst in inp:
+        meritve.append(int(vrst))
+
+okna = []
+for j in range(len(meritve)-2):
+    okna.append(meritve[j] + meritve[j+1] + meritve[j+2])
+
+def count_increase(sez):
+    count = 0
+    for i in range(1, len(sez)):
+        if sez[i] > sez[i-1]:
+            count += 1
+    return count
+
+# --------------------------
+
+print("1. del: " + str(count_increase(meritve)))
+print("2. del: " + str(count_increase(okna)))
+
+
+
